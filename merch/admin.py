f8f34acb22f773487137acf659f6db54ca8d0f62
Charles Thompson <thompsoncharles@live.com>
2018-07-28 11:39:39 +0000

    fix(merch): cat issues

--- merch/admin.py
+++ merch/admin.py
@@ -1,1 +1,11 @@
 # Register your models here.
+from django.contrib import admin
+
+from merch.models import Merch
+
+
+class MerchAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Merch, MerchAdmin)
