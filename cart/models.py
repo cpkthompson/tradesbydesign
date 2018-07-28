2287c9be15fa2891b07d97438e21472e56d29789
Charles Thompson <thompsoncharles@live.com>
2018-07-28 15:22:38 +0000

    toil

--- cart/models.py
+++ cart/models.py
@@ -1,1 +1,14 @@
 # Create your models here.
+from django.conf import settings
+from django.db import models
+
+from merch.models import Merch
+
+
+class Cart(models.Model):
+    quantity = models.IntegerField(default=1)
+    item = models.ForeignKey(Merch, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='buyer', blank=True, null=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- cart/views.py
+++ cart/views.py
@@ -1,34 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect
 
+from cart.models import Cart
 from merch.models import Merch
-from .cart import Cart
-from .forms import CartAddProductForm
 
 
-@require_POST
 def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Merch, id=product_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product,
-                 quantity=cd['quantity'],
-                 update_quantity=cd['update'])
+    Cart.objects.create(user=request.user, item=Merch.objects.get(id=product_id), quantity=1)
     return redirect('cart:cart_detail')
 
 
-def cart_remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Merch, id=product_id)
-    cart.remove(product)
+def cart_remove(request, item_id):
+    cart = Cart.objects.get(id=item_id)
+    cart.delete()
     return redirect('cart:cart_detail')
 
 
 def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
-                                                                   'update': True})
+    cart = Cart.objects.filter(user=request.user)
     return render(request, 'cart/cart.html', {'cart': cart})
