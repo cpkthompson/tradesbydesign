fb1ab815648037486619177758c8fb123443947b
Charles Thompson <thompsoncharles@live.com>
2018-07-28 09:22:49 +0000

    fix(url): url issues

--- tradesbydesign/urls.py
+++ tradesbydesign/urls.py
@@ -5,14 +5,16 @@ from django.utils.translation import gettext_lazy as _
 from accounts.views import profile
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
-    url(_(r'^accounts/'), include('allauth.urls')),
-    url(_(r'^accounts/profile/$'), profile, name='profile'),
-    url(_(r'^merch/'), include('merch.urls', namespace='merch')),
     # url(_(r'^cart/'), include('cart.urls', namespace='cart')),
     # url(_(r'^orders/'), include('orders.urls', namespace='orders')),
     # url(_(r'^payment/'), include('payment.urls', namespace='payment')),
     # url(_(r'^collections/'), include('collection.urls', namespace='collections')),
-    url(_(r'^'), include('frontend.urls', namespace='frontend'))
+    url(_(r'^accounts/'), include('allauth.urls')),
+    url(r'^accounts/profile/$', profile, name='profile'),
+    url(_(r'^merch/'), include('merch.urls', namespace='merch')),
+    url(r'^', include('frontend.urls', namespace='frontend')),
+
 
 ]
